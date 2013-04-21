38ddc7677aba6cf91cedc8c49a43097f143ef327
Paul Durbaba <paul.durbaba@btinternet.com>
2013-04-21 14:54:13 +0100

    Added missing file PackageNode

---
+++ VC4 Launcher/src/vc4/launcher/gui/PackageNode.java
@@ -0,0 +1,17 @@
+package vc4.launcher.gui;
+
+import vc4.launcher.Package;
+import vc4.launcher.gui.settings.PackageSettingsPanel;
+
+public class PackageNode extends SettingsNode {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3079007254693466614L;
+
+	public PackageNode(Package pack) {
+		super(pack.getName(), new PackageSettingsPanel(pack));
+	}
+
+}
