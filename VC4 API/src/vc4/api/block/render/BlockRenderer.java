fd76be826bf7ab76fef15ae04035e5ee0267e4db
Paul Durbaba <paul.durbaba@btinternet.com>
2013-05-09 14:19:16 +0200

    Update BlockRenderer.java
    Added method for rendering cracks

--- VC4 API/src/vc4/api/block/render/BlockRenderer.java
+++ VC4 API/src/vc4/api/block/render/BlockRenderer.java
@@ -7,6 +7,7 @@ import vc4.api.block.Block;
 import vc4.api.graphics.Renderer;
 import vc4.api.item.ItemStack;
 import vc4.api.world.Chunk;
+import vc4.api.world.World;
 
 /**
  * @author paul
@@ -16,4 +17,5 @@ public interface BlockRenderer {
 
 	public void renderBlock(Chunk c, int cx, int cy, int cz, Block block, byte data, Renderer[] renderers);
 	public void renderBlock(ItemStack block, float x, float y, float z, Renderer render);
+	public void renderBlockCracks(World world, long x, long y, long z, Renderer render);
 }
