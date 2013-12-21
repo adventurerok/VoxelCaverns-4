74c0d39ee2c4eabee95e6ae642c260ab83980adf
Paul Durbaba <paul.durbaba@btinternet.com>
2013-12-21 21:43:22 +0000

    Found and included missing java files

---
+++ VC4 Vanilla Plugin/src/vc4/vanilla/underbiome/BiomeGenUnderBiomes.java
@@ -0,0 +1,30 @@
+package vc4.vanilla.underbiome;
+
+import vc4.api.biome.ZoomGenerator;
+import vc4.api.world.World;
+
+public class BiomeGenUnderBiomes extends ZoomGenerator {
+
+	int[] biomesToGenerate;
+	
+	@Override
+	public int[] generate(long x, long z, int size) {
+		int[] result;
+		if(parent != null) result = parent.generate(x, z, size);
+		else result = new int[size * size];
+		for(int px = 0; px < size; ++px){
+			for(int pz = 0; pz < size; ++pz){
+				initSeed(x + px, z + pz);
+				result[pz * size + px] = biomesToGenerate[nextInt(biomesToGenerate.length)];
+			}
+		}
+		return result;
+	}
+
+	public BiomeGenUnderBiomes(World world, int[] biomes) {
+		super(world);
+		this.biomesToGenerate = biomes;
+		
+	}
+
+}
---
+++ VC4 Vanilla Plugin/src/vc4/vanilla/underbiome/UnderBiome.java
@@ -0,0 +1,80 @@
+package vc4.vanilla.underbiome;
+
+import java.awt.Color;
+
+import vc4.api.sound.Music;
+
+public class UnderBiome {
+
+	private static UnderBiome[] biomesList = new UnderBiome[256];
+	
+	public Color mapColor;
+	public int id;
+	public int stoneBlockId;
+	public byte stoneBlockData;
+	public int cavesChance = 115;
+	public int cavesAmount = 7;
+	public String name;
+	public Music music = null;
+	
+	public static UnderBiome[] getBiomesList() {
+		return biomesList.clone();
+	}
+	
+	public UnderBiome(int id, String name, Color mapColor) {
+		super();
+		this.mapColor = mapColor;
+		this.name = name;
+		this.id = id;
+		biomesList[id] = this;
+	}
+	
+
+	public Color getMapColor() {
+		return mapColor;
+	}
+	
+	public UnderBiome setStoneBlock(int stoneBlockId, int data) {
+		this.stoneBlockId = stoneBlockId;
+		this.stoneBlockData = (byte) data;
+		return this;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public UnderBiome setCaveData(int chance, int amount){
+		if(chance < 55) chance = 55;
+		this.cavesChance = chance;
+		this.cavesAmount = amount;
+		return this;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public Music getMusic() {
+		return music;
+	}
+
+	public static UnderBiome byId(int id){
+		return biomesList[id];
+	}
+	
+
+	
+	private UnderBiome(){
+		
+	}
+	
+	public static UnderBiome createFake(){
+		return new UnderBiome();
+	}
+	
+	
+
+	
+	
+}
